1e3a4d54e542c6230144b047b85a5dc238791ff9
JaydenHuang555 <JaydenkanaHuang@outlook.com>
2025-11-28 08:55:47 -0800

    port args

---
+++ src/arg_util.rs
@@ -0,0 +1,20 @@
+use std::{
+    env::{args, Args}
+};
+
+pub fn get_args() -> Args {
+    args()
+}
+
+pub fn get_args_str_buffer() -> Vec<String> {
+    let raw_args = get_args();
+    let mut buffer = Vec::<String>::new();
+    for arg in raw_args {
+        buffer.push(String::from(arg.as_str()));
+    }
+    buffer
+}
+
+pub struct ArgToken {
+
+}
