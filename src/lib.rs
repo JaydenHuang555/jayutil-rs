9141b3ed9a381c5480a0235036f0540981da4af1
JaydenHuang555 <JaydenkanaHuang@outlook.com>
2025-11-28 10:44:50 -0800

    tests

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,35 @@ pub mod function;
 
 #[cfg(test)]
 mod tests {
+    use std::iter::Inspect;
+
+    use crate::function::UnaryOperator;
+
     use super::*;
 
     #[test]
-    fn it_works() {}
+    fn supplier() {
+        let supplier: function::Supplier<String> = | | return "input".to_string();
+        assert!(supplier().eq("input"));
+    }
+
+    #[test]
+    fn consumer() {
+        let consumer: function::Consumer<&mut Option<String>> = |x: &mut Option<String> | {
+           x.take(); 
+        };
+        let input = &mut Option::Some("input".to_string());
+        consumer(input);
+        assert!(input.is_none());
+    }
+
+    #[test]
+    fn unary_operator() {
+        let operation: UnaryOperator<u32> = |x| return x * 2;
+        let input = 3;
+        let output = operation(input);
+        println!("{}", output);
+        assert!(output == (input * 2));
+    }
+
 }
