c3d55296ac91f3293aff3393cd5b797b6fdda991
JaydenHuang555 <JaydenKanaHuang@outlook.com>
2025-12-06 00:45:53 -0800

    time errors for error handling

--- src/time/time.rs
+++ src/time/time.rs
@@ -1,14 +1,29 @@
-use std::{alloc::System, time::{SystemTime, UNIX_EPOCH}};
+use std::{alloc::System, error::Error, fmt::Display, time::{Instant, SystemTime, UNIX_EPOCH}};
 
+#[derive(Debug)]
+pub enum TimeError {
+    InvalidEpoch
+}
 
-pub fn get_epoch() -> Result<f64, ()> {
+impl Error for TimeError {}
+
+impl Display for TimeError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::InvalidEpoch => writeln!(f, "fetched epoch value is invalid")
+            
+        }
+    }
+}
+
+pub fn get_epoch() -> Result<f64, TimeError> {
     let now = SystemTime::now();
     match now.duration_since(UNIX_EPOCH) {
         Ok(time) => {
             Result::Ok(time.as_secs_f64())
         }
         Err(_) => {
-            Result::Err(())
+            Result::Err(TimeError::InvalidEpoch)
         },
     }
 }
