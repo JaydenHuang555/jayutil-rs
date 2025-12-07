2d6b2ea1ada2fdafe777da354c88423541e26374
JaydenHuang555 <JaydenKanaHuang@outlook.com>
2025-12-06 20:14:15 -0800

    num-type

---
+++ src/math/unit.rs
@@ -0,0 +1,34 @@
+
+use core::fmt::{Debug, Display};
+
+pub trait NumLike: core::ops::Add<Output=Self>
++ core::ops::AddAssign
++ core::ops::Sub<Output=Self>
++ core::ops::SubAssign
++ core::ops::Mul<Output=Self>
++ core::ops::MulAssign
++ core::ops::Div<Output=Self>
++ core::ops::DivAssign
++ core::ops::Neg<Output=Self>
++ PartialOrd
++ PartialEq
++ Clone
++ Debug
++ Display
+{}
+
+impl<T> NumLike for T where T: core::ops::Add<Output=Self>
++ core::ops::AddAssign
++ core::ops::Sub<Output=Self>
++ core::ops::SubAssign
++ core::ops::Mul<Output=Self>
++ core::ops::MulAssign
++ core::ops::Div<Output=Self>
++ core::ops::DivAssign
++ core::ops::Neg<Output=Self>
++ PartialOrd
++ PartialEq
++ Clone
++ Debug
++ Display
+{}
