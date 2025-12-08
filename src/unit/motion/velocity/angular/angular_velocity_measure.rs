4bc33baf7f8cad36b1dc2737974fc076e2dd3bc0
JaydenHuang555 <JaydenKanaHuang@outlook.com>
2025-12-08 09:58:47 -0800

    angular velocity

---
+++ src/unit/motion/velocity/angular/angular_velocity_measure.rs
@@ -0,0 +1,9 @@
+use crate::{jayutil_unit_generate_measure_impl, jayutil_unit_generate_measure_traits, unit::motion::velocity::angular::angular_velocity_unit::AngularVelocityUnit};
+
+
+pub struct AngularVelocity<Num> {
+    base: Num
+}
+
+jayutil_unit_generate_measure_impl!(AngularVelocity, AngularVelocityUnit);
+jayutil_unit_generate_measure_traits!(AngularVelocity);
