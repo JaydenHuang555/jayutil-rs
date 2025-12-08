f38b66f224d4a79e292ae94fd499dcac60385714
JaydenHuang555 <JaydenKanaHuang@outlook.com>
2025-12-08 10:19:09 -0800

    get a ref

--- src/unit/measure.rs
+++ src/unit/measure.rs
@@ -6,12 +6,12 @@ pub trait Measure<Num: NumLike, U: Unit> {
     fn set_base(&mut self, base: Num);
     fn get_base(&self) -> Num;
 
-    fn set(&mut self, value: Num, unit: U) -> &Self {
+    fn set(&mut self, value: Num, unit: &U) -> &Self {
         self.set_base(unit.to_base(value));
         self
     }
 
-    fn to(&self, unit: U) -> Num {
+    fn to(&self, unit:&U) -> Num {
         unit.from_base(self.get_base())
     }
 }
